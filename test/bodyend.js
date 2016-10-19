6aef3707c73630ae3740f8b56bc71b36a4b4a19b
unknown <lig@TRGAME-LIG.uc888.cn>
2016-10-19 17:47:07 +0800

    seperate script with html

---
+++ test/bodyend.js
@@ -0,0 +1,1 @@
+alert($("embed").attr("src"));
---
+++ test/head.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    console.log("doc loaded");
+});
+$(window).on('load', function () {
+    console.log("window loaded");
+});
